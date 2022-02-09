f085c8e5d3bea7633bc0baf9eab012ad6e2dd9cd
UcheOsujiBison <91709768+UcheOsujiBison@users.noreply.github.com>
2022-02-09 18:54:11 -0500

    Added Unit 2 Work

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Uche Osuji on 2/5/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
